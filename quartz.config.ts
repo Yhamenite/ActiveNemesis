1d785c07060bff02bb472cadc317416af14b5afb
Yamen <ymnmaj@outlook.com>
2024-06-19 23:41:30 +0300

    Quartz sync: Jun 19, 2024, 11:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -2,10 +2,12 @@ import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
+ * 
  * Quartz 4.0 Configuration
  *
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
+
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "ActiveNemesis 💀",
